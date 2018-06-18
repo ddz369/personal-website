3ddabee8fb9588da5879c50d92b858c146fc1b72
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-18 13:30:18 -0600

    Updated the contact part to have a proper form and input fields/buttons.

--- templates/src/components/Contact/Contact.js
+++ templates/src/components/Contact/Contact.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './style.css'
 
+const INITIAL_STATE = {
+    email: '',
+    subject: '',
+    comment: '',
+    error: null,
+}
+
 const Contact = () =>
   <div className="contact">
-    <h1>Hello World!</h1>
+    <form>
+      <label for="name-field">email</label>
+      <input type="text" id="name-field"/>
+      <label for="subject-field">subject</label>
+      <input type="text" id="subject-field"/>
+      <label for="comment-field">comment</label>
+      <textarea id="comment-field" rows="10"></textarea>
+      <button> send email </button>
+      <button> clear </button>
+    </form>
   </div>
 
 export default Contact
