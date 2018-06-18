1833990fd06dc8fc93904215682781c451184bef
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-18 08:31:37 -0600

    Added proper routing via the nav-bar. Created the "about" page.

--- templates/src/components/App.js
+++ templates/src/components/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import avatar from '../images/avatar.png';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 
+import * as routes from '../constants/routes'
 import Navigation from './Navigation'
 import Footer from './Footer'
+import About from './About'
+
+import './App.css';
+import avatar from '../images/avatar.png';
 
 class App extends Component {
   render() {
     return (
+    <BrowserRouter>
       <div className="main-container">
         <header className="navigation-container">
           <Navigation />
         </header>
         <body className="content-container">
           <div className="content">
+            <Route exact path={routes.ABOUT} component={About} />
             <img src={avatar} className="avatar"/>
             <h1>hello, i'm <span>darvin</span>.</h1>
             <h4>please be patient. the site is under construction.<br/>
@@ -24,6 +30,7 @@ class App extends Component {
           <Footer/>
         </footer>
       </div>
+    </BrowserRouter>
     );
   }
 }
--- templates/src/components/Navigation/Navigation.js
+++ templates/src/components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import * as routes from '../../constants/routes'
 import './style.css'
 
 const Navigation = () =>
   <div className="nav-bar">
-    <a href="#">about</a>&ensp;|&ensp;
-    <a href="#">portfolio</a>&ensp;|&ensp;
-    <a href="#">contact</a>
+    <Link to={routes.ABOUT}>about</Link>&ensp;|&ensp;
+    <Link to={routes.PORTFOLIO}>portfolio</Link>&ensp;|&ensp;
+    <Link to={routes.CONTACT}>contact</Link>
   </div>
 
 export default Navigation
