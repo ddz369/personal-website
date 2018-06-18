1c2b7d00e9d10af18cb481b0e68624c2bcd6590b
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-17 21:56:05 -0600

    Started the navigation bar. Installed react icons.

--- templates/src/components/App.js
+++ templates/src/components/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import './App.css';
 
+import Navigation from './Navigation'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="main-container">
+        <header className="header-container">
+          <Navigation />
         </header>
+        <body className="content-container">
+        <h1 className="App-title">Welcome to my Site!</h1>
+        </body>
+        <footer className="footer-container">
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </footer>
       </div>
     );
   }
---
+++ templates/src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+const Navigation = () =>
+  <div className="navbar">
+    <a href="#">News</a>
+    <a href="">Contact</a>
+    <a href="#">About</a>
+  </div>
+
+export default Navigation
