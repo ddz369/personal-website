851f224a06745f4dec0c0b394c1d0225429c22a2
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-18 10:10:00 -0600

    Updated site's icon and title.

--- templates/src/components/Navigation/Navigation.js
+++ templates/src/components/Navigation/Navigation.js
@@ -5,9 +5,9 @@ import './style.css'
 
 const Navigation = () =>
   <div className="nav-bar">
-    <NavLink to={routes.ABOUT} activeStyle={{ textDecoration: 'underline'}}>about</NavLink>&ensp;|&ensp;
-    <NavLink to={routes.PORTFOLIO} activeStyle={{ textDecoration: 'underline' }}>portfolio</NavLink>&ensp;|&ensp;
-    <NavLink to={routes.CONTACT} activeStyle={{ textDecoration: 'underline' }}>contact</NavLink>
+    <NavLink to={routes.ABOUT} activeStyle={{ borderBottom: '1px solid white'}}>about</NavLink>&ensp;|&ensp;
+    <NavLink to={routes.PORTFOLIO} activeStyle={{ borderBottom: '1px solid white' }}>portfolio</NavLink>&ensp;|&ensp;
+    <NavLink to={routes.CONTACT} activeStyle={{ borderBottom: '1px solid white' }}>contact</NavLink>
   </div>
 
 export default Navigation
