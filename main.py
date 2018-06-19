5a1df5be06ef47b195c0d598f4cd102c2df01fa5
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-19 01:42:57 -0600

    Added and tested the email sending utility "sendgrid". Implemented a class that allows us to receive contacts. Added references of our new routes to our python back-end.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/about/')
+@app.route('/portfolio/')
+@app.route('/contact/')
 def hello_world():
     return render_template('build/index.html')
 
---
+++ sendgrid.py
@@ -0,0 +1,23 @@
+import sendgrid
+# hidden secret API key that git ignores
+from sendgrid_key import *
+from sendgrid.helpers.mail import *
+
+receiver = "darvin.zhang@gmail.com"
+
+# function that looks to send an email via sendgrid
+def send(sender, subject, comment):
+    # initializing the sendgrid class with our api key
+    sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+
+    # mail declaration
+    from_email = Email(sender)
+    to_email = Email(receiver)
+    content = Content("text/plain", comment)
+    mail = Mail(from_email, subject, to_email, content)
+
+    # attempt to send the mail
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
