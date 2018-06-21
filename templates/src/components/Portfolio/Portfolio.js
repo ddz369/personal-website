86d9a121081b65ff3aee5c7494ef4d398f87d760
Darvin Zhang <darvin.zhang@usask.ca>
2018-06-20 18:21:18 -0600

    Added a new font to potentially use. Portfolio often was halted by load times, so I added a loader to it.

--- templates/src/components/Portfolio/Portfolio.js
+++ templates/src/components/Portfolio/Portfolio.js
@@ -10,42 +10,59 @@ import videoroulette from '../../images/videoroulette.png'
 import gamejam2015 from '../../images/gamejam2015.png'
 import gamejam2016 from '../../images/gamejam2016.png'
 
-const Portfolio = () =>
-  <div className="portfolio" alt="">
-    <ul>
-      <li>
-        <h2>lynkcoin website</h2>
-        <img src={lynkcoin} alt=""/>
-      </li>
-      <li>
-        <h2>qm-lab app</h2>
-        <img src={qmlab} alt=""/>
-      </li>
-      <li>
-        <h2>videoroulette</h2>
-        <img src={videoroulette} alt=""/>
-      </li>
-      <li>
-        <h2>phonenanza HCI</h2>
-        <img src={phonenanza} alt=""/>
-      </li>
-      <li>
-        <h2>robot fighting simulator</h2>
-        <img src={stackwars} alt=""/>
-      </li>
-      <li>
-        <h2>multi-language chatroom app</h2>
-        <img src={chatapp} alt=""/>
-      </li>
-      <li>
-        <h2>phage</h2>
-        <img src={gamejam2015} alt=""/>
-      </li>
-      <li>
-      <h2>arena fighter</h2>
-      <img src={gamejam2016} alt=""/>
-      </li>
-    </ul>
-  </div>
+class Portfolio extends React.Component {
+    state = { loading: true };
+
+    componentDidMount() {
+        setTimeout(() => this.setState({ loading: false }), 1500);
+    }
+
+    render() {
+    const { loading } = this.state;
+
+    if (loading) {
+        return null;
+    }
+
+    return (
+      <div className="portfolio" alt="">
+        <ul>
+          <li>
+            <h2>lynkcoin website</h2>
+            <img src={lynkcoin} alt=""/>
+          </li>
+          <li>
+            <h2>qm-lab app</h2>
+            <img src={qmlab} alt=""/>
+          </li>
+          <li>
+            <h2>videoroulette</h2>
+            <img src={videoroulette} alt=""/>
+          </li>
+          <li>
+            <h2>phonenanza HCI</h2>
+            <img src={phonenanza} alt=""/>
+          </li>
+          <li>
+            <h2>robot fighting simulator</h2>
+            <img src={stackwars} alt=""/>
+          </li>
+          <li>
+            <h2>multi-language chatroom app</h2>
+            <img src={chatapp} alt=""/>
+          </li>
+          <li>
+            <h2>phage</h2>
+            <img src={gamejam2015} alt=""/>
+          </li>
+          <li>
+          <h2>arena fighter</h2>
+          <img src={gamejam2016} alt=""/>
+          </li>
+        </ul>
+      </div>
+      );
+    }
+}
 
 export default Portfolio
